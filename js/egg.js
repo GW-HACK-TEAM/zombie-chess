55ff474fa436aa9b0cc777f30de4f054cfe01dc0
Lucian Buzzo <lucian@gravitywell.co.uk>
2016-11-24 20:09:18 +0000

    FLIPPIN FLAGS!!!

--- js/egg.js
+++ js/egg.js
@@ -13,6 +13,7 @@
     }
 
     if (text.indexOf('drake') > -1) {
+      backgroundSound.stop();
       loaded = true;
       console.log('DRAKE IT BABY!');
       $('body').html('<img style="margin: 100px auto 0; display: block" src="/img/drake.gif" />')
