dbf0315fa60edf72fbdf4f35dc3a0ce98e3b2098
Luke Baker <luke@gravitywell.co.uk>
2016-11-24 16:21:12 +0000

    Fixed issue where captured would not show the captor and captured correctly for the corrisponding team

--- js/enginegame.js
+++ js/enginegame.js
@@ -77,7 +77,7 @@ const engineGame = function engineGame(options) {
     if (game.game_over()) {
       announced_game_over = true;
 
-      alert("Your game is only FUCKING OVER!");
+      EventsLib.gameOver();
     }
   }, 1000);
 
@@ -225,7 +225,7 @@ const engineGame = function engineGame(options) {
   };
 
   engine.onmessage = function(event) {
-    console.log('engine:', event);
+    // console.log('engine:', event);
     var line;
     var match;
 
